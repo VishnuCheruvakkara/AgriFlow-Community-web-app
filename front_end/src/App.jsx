41f0af850a37cd92637fc803603007eae1da4736
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-03-08 10:09:08 +0530

    completed the login based on google and error based on time dely was fixed toast message implimenttion completed for google based authentication!

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import AppRoutes from './routes/routes'
+import ToastNotifications from './components/toast-notification/CustomToast'
 
 function App() {
   return (
-      <AppRoutes/>
+    <div>
+      <ToastNotifications />
+      <AppRoutes />
+    </div>
   )
 }
 
--- front_end/src/components/Authentication/GoogleAuthButton.jsx
+++ front_end/src/components/Authentication/GoogleAuthButton.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { loginSuccess } from "../../redux/slices/AuthSlice"
+import { showToast } from "../toast-notification/CustomToast"
+
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
@@ -18,6 +20,9 @@ const GoogleAuthButton = () => {
                 token: credential,
             });
 
+            
+            console.log("Backend Response:", response.data); // Debugging
+
             const { user, access_token } = response.data; // Extract user & token
 
             // Store token in local storage
@@ -25,17 +30,21 @@ const GoogleAuthButton = () => {
 
             // Dispatch Redux action to store user data
             dispatch(loginSuccess({ user, token: access_token }));
-
+            showToast(`Welcome ${user.username}! Login successful`,"success")
+            
             // Navigate to the user dashboard
             navigate("/user-dash-board");
+          
 
         } catch (error) {
             console.error("Google Auth Failed", error);
+            showToast("Google login failed. Please try again.", "error");
         }
     };
 
     const handleFailure = () => {
         console.error("Google Login Failed");
+        showToast("Google Login Failed!", "warn");
     };
 
     return (
