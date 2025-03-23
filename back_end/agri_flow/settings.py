5d04dc1f346cd91daec225f8b40e660420271224
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-03-23 12:16:09 +0530

    Profile updateion on progress

--- back_end/agri_flow/settings.py
+++ back_end/agri_flow/settings.py
@@ -92,7 +92,7 @@ REST_FRAMEWORK = {
 #################### JWT Token custom setup. ############################
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
     'ROTATE_REFRESH_TOKENS': True,
