a97169308a25d5824778812e9536fb379fb992e7
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-03-29 15:02:52 +0530

    Conflict solved in the UserLayOutFile.

--- front_end/src/layout/UserLayout.jsx
+++ front_end/src/layout/UserLayout.jsx
@@ -60,6 +60,7 @@ const UserLayout = () => {
 
             )}
 
+
             {/* Footer section */}
             <Footer />
 
--- front_end/src/pages/admin-dash-board/UserPage.jsx
+++ front_end/src/pages/admin-dash-board/UserPage.jsx
@@ -1,31 +1,95 @@
 import React from "react";
 
-
 const UsersPage = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold text-secondary">User Management</h2>
-      <table className="table w-full mt-4">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>John Doe</td>
-            <td>john@example.com</td>
-            <td>Admin</td>
-          </tr>
-          <tr>
-            <td>Jane Smith</td>
-            <td>jane@example.com</td>
-            <td>User</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="p-6 bg-white rounded-lg shadow-sm">
+      <h2 className="text-2xl font-bold text-green-600">User Management</h2>
+
+
+
+
+      <div className="mt-8 grid grid-cols-1  gap-6">
+        <div className="bg-white p-6 rounded-lg border border-gray-100 shadow-sm">
+          <h3 className="font-bold text-gray-700 mb-4">Recent Orders</h3>
+          <div className="overflow-x-auto">
+
+
+
+
+            <table class="min-w-full divide-y divide-gray-200 overflow-x-auto">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Name
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Title
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Status
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Role
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Email
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <tr>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                      <div class="flex-shrink-0 h-10 w-10">
+                        {/* <img class="h-10 w-10 rounded-full" src="https://i.pravatar.cc/150?img=1" alt=""> */}
+                      </div>
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-gray-900">
+                          Jane Cooper
+                        </div>
+                        <div class="text-sm text-gray-500">
+                          jane.cooper@example.com
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
+                    <div class="text-sm text-gray-500">Optimization</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      Active
+                    </span>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    Admin
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    jane.cooper@example.com
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap  text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                    <a href="#" class="ml-2 text-red-600 hover:text-red-900">Delete</a>
+                  </td>
+                </tr>
+
+
+
+              </tbody>
+            </table>
+
+
+
+
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 };
