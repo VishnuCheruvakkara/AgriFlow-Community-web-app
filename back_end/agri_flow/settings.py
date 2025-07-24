f8d5588cfbb6b4dd084c67f10cdcfaa830269633
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.coms>
2025-07-25 01:45:06 +0530

    GitHub secret setup completed for both the Repository secrets and Repository variables successfully

--- back_end/agri_flow/settings.py
+++ back_end/agri_flow/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','web']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # path to all apps that are inside the common 'apps' folder.
 APPS_DIR = BASE_DIR / 'apps'
